9d68085e8ab4e5fabf86e2aac5e64cb5c9ae5d31
Beau Yancey <yancey.beau.j@gmail.com>
2024-09-26 20:34:37 -0600

    refactor(tests) utility functions

--- src/database/database.test.js
+++ src/database/database.test.js
@@ -2,44 +2,9 @@ const { StatusCodeError } = require('../endpointHelper');
 const { DB, Role } = require('./database');
 const jwt = require('jsonwebtoken');
 const config = require('../config')
+const {createDiner, createAdmin, randomString} = require('../utils')
 
 jest.setTimeout(5 * 60 * 1000)
-
-function randomString() {
-	return Math.random().toString(36).substring(2, 12)
-}
-
-async function createAdmin() {
-	const name = randomString()
-  const newUser = { 
-		name,
-		email: name + '@admin.com',
-		password: 'verysecurepassword',
-		roles: [{ role: Role.Admin }]
-	};
-
-  const result = await DB.addUser(newUser);
-	newUser.id = result.id
-	newUser.password = 'verysecurepassword'
-
-  return newUser;
-}
-
-async function createDiner() {
-	const name = randomString()
-  const newUser = { 
-		name,
-		email: name + '@diner.com',
-		password: 'verysecurepassword',
-		roles: [{ role: Role.Diner }]
-	};
-
-  const result = await DB.addUser(newUser);
-	newUser.id = result.id
-	newUser.password = 'verysecurepassword'
-
-  return newUser;
-}
 
 test('add menu item', async () => {
 	const honeySerrano = {title: 'honey serrano', description: 'sweet and spicy', image: 'honey.png', price: 0.002}
