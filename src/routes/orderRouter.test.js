c927f24c14488f6128e4461e685e13b60f36bfc0
Beau Yancey <yancey.beau.j@gmail.com>
2024-09-27 19:33:02 -0600

    test(orderRouter) order endpoints

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -52,3 +52,82 @@ test('add menu item 403', async () => {
 	expect(addRes.status).toBe(403)
 	expect(after.body.length).toBe(before.body.length)
 })
+
+test('create order', async () => {
+
+	global.fetch = async () => ({
+		status: 200,
+		ok: true,
+		json: async () => ({
+			jwt: 'part1.part2.part3',
+			reportUrl: 'pizza.yanceydev.com'
+		})
+	})
+
+	const honeySerrano = {title: 'honey serrano', description: 'sweet and spicy', image: 'honey.png', price: 0.002}
+	const menu = await request(app)
+		.put('/api/order/menu')
+		.set({Authorization: `Bearer ${adminToken}`})
+		.send(honeySerrano)
+	const order = {franchiseId: store.franchiseId, storeId: store.id, items: [{menuId: menu.body[0].id, description: menu.body[0].description, price: menu.body[0].price}]}
+	const addRes = await request(app)
+		.post('/api/order')
+		.set({Authorization: `Bearer ${dinerToken}`})
+		.send(order)
+
+	expect(addRes.status).toBe(200)
+	expect(addRes.body.jwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/)
+})
+
+test('create order factory failure', async () => {
+	global.fetch = async () => ({
+		status: 500,
+		ok: false,
+		json: async () => ({
+			reportUrl: 'pizza.yanceydev.com'
+		})
+	})
+
+	const honeySerrano = {title: 'honey serrano', description: 'sweet and spicy', image: 'honey.png', price: 0.002}
+	const menu = await request(app)
+		.put('/api/order/menu')
+		.set({Authorization: `Bearer ${adminToken}`})
+		.send(honeySerrano)
+	const order = {franchiseId: store.franchiseId, storeId: store.id, items: [{menuId: menu.body[0].id, description: menu.body[0].description, price: menu.body[0].price}]}
+	const addRes = await request(app)
+		.post('/api/order')
+		.set({Authorization: `Bearer ${dinerToken}`})
+		.send(order)
+
+	expect(addRes.status).toBe(500)
+	expect(addRes.body).toMatchObject({ message: 'Failed to fulfill order at factory', reportUrl: 'pizza.yanceydev.com' })
+})
+
+test('get orders', async () => {
+	global.fetch = async () => ({
+		status: 200,
+		ok: true,
+		json: async () => ({
+			jwt: 'part1.part2.part3',
+			reportUrl: 'pizza.yanceydev.com'
+		})
+	})
+
+	const honeySerrano = {title: 'honey serrano', description: 'sweet and spicy', image: 'honey.png', price: 0.002}
+	const menu = await request(app)
+		.put('/api/order/menu')
+		.set({Authorization: `Bearer ${adminToken}`})
+		.send(honeySerrano)
+	const order = {franchiseId: store.franchiseId, storeId: store.id, items: [{menuId: menu.body[0].id, description: menu.body[0].description, price: menu.body[0].price}]}
+	const addRes = await request(app)
+		.post('/api/order')
+		.set({Authorization: `Bearer ${dinerToken}`})
+		.send(order)
+	const getRes = await request(app)
+		.get('/api/order')
+		.set({Authorization: `Bearer ${dinerToken}`})
+
+	expect(getRes.status).toBe(200)
+	expect(getRes.body.dinerId).toBe(diner.id)
+	expect(getRes.body.orders.length).toBeGreaterThan(0)
+})
