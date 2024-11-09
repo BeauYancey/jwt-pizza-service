7fe8d375f64f9f4bbd78452d40a143df394eecdf
Beau Yancey <yancey.beau.j@gmail.com>
2024-11-09 15:57:45 -0700

    add(metrics) request totals and os metrics

---
+++ src/metrics.js
@@ -0,0 +1,134 @@
+const config = require('./config')
+const os = require('os')
+
+class Metrics {
+	constructor() {
+		this.requests = {
+			all: 0,
+			get: 0,
+			put: 0,
+			post: 0,
+			delete: 0
+		}
+		this.activeUsers = 0
+		this.authAttempts = {
+			all: 0,
+			success: 0,
+			fail: 0
+		}
+		this.pizzas = {
+			sold: 0,
+			creationFailures: 0,
+			revenue: 0
+		}
+		const timer = setInterval(() => {
+			this.sendHTTPMetricsToGrafana()
+			this.sendAuthMetricsToGrafana() // TODO
+			this.sendPizzaMetricsToGrafana() // TODO
+			this.sendMetricToGrafana(this.createMetricString('user', [], 'total', this.activeUsers)) // TODO
+			this.sendOSMetricsToGrafana()
+		}, 10000)
+		timer.unref()
+	}
+
+	requestTracker(req, res, next) {
+		this.requests.all++
+		this.requests[req.method.toLowerCase()]++
+		next()
+	}
+
+	logAuthAttempt(success) {
+		this.authAttempts.all++
+		if (success) {
+			this.authAttempts.success++
+			this.activeUsers++
+		}
+		else {
+			this.authAttempts.fail++
+		}		
+	}
+
+	logLogout() {
+		this.activeUsers--
+	}
+
+	logPizza(revenue) {
+		this.pizzas.sold++
+		this.pizzas.revenue += revenue
+	}
+
+	logPizzaFailure() {
+		this.pizzas.creationFailures++
+	}
+
+	sendHTTPMetricsToGrafana() {
+		const allMetrics = this.createMetricString('request', [['method', 'all']], 'total', this.requests.all) + `\n` + 
+			this.createMetricString('request', [['method', 'get']], 'total', this.requests.get) + `\n` + 
+			this.createMetricString('request', [['method', 'post']], 'total', this.requests.post) + `\n` + 
+			this.createMetricString('request', [['method', 'put']], 'total', this.requests.put) + `\n` + 
+			this.createMetricString('request', [['method', 'delete']], 'total', this.requests.delete)
+		this.sendMetricToGrafana(allMetrics)
+	}
+
+	sendAuthMetricsToGrafana() {
+		const allMetrics = this.createMetricString('auth', [['status', 'all']], 'total', this.authAttempts.all) + `\n` + 
+			this.createMetricString('auth', [['status', 'success']], 'total', this.authAttempts.success) + `\n` + 
+			this.createMetricString('auth', [['status', 'fail']], 'total', this.authAttempts.fail)
+		this.sendMetricToGrafana(allMetrics)
+	}
+
+	sendPizzaMetricsToGrafana() {
+		const allMetrics = this.createMetricString('pizza', [['status', 'sold']], 'total', this.pizzas.sold) + `\n` + 
+			this.createMetricString('pizza', [['status', 'creation_failure']], 'total', this.pizzas.creationFailures) + `\n` + 
+			this.createMetricString('pizza', [], 'revenue', this.pizzas.revenue)
+		this.sendMetricToGrafana(allMetrics)
+	}
+
+	sendOSMetricsToGrafana() {
+		const cpu = getCpuUsagePercentage()
+		const mem = getMemoryUsagePercentage()
+		const metricString = this.createMetricString('system', [], 'cpu', cpu) + `\n` + 
+			this.createMetricString('system', [], 'memory', mem)
+		this.sendMetricToGrafana(metricString)
+	}
+
+	createMetricString(metricPrefix, tags, metricName, metricValue) {
+		const tagStrings = tags.map(([tagName, tagValue]) => `${tagName}=${tagValue}`)
+		const tagString = tagStrings.join(',')
+		return `${metricPrefix},source=${config.metrics.source}${tagString && ','}${tagString} ${metricName}=${metricValue}`
+	}
+
+	sendMetricToGrafana(metricString) {
+		console.log(`sending ${metricString}`)
+		fetch(`${config.metrics.url}`, {
+      method: 'post',
+      body: metricString,
+      headers: { Authorization: `Bearer ${config.metrics.userId}:${config.metrics.apiKey}` },
+    })
+		.then((response) => {
+			if (!response.ok) {
+				console.error('Failed to push metrics data to Grafana');
+			}
+		})
+		.catch((error) => {
+			console.error('Error pushing metrics:', error);
+		});
+	}
+}
+
+function getCpuUsagePercentage() {
+  const cpuUsage = os.loadavg()[0] / os.cpus().length;
+  return cpuUsage.toFixed(2) * 100;
+}
+
+function getMemoryUsagePercentage() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const memoryUsage = (usedMemory / totalMemory) * 100;
+  return memoryUsage.toFixed(2);
+}
+
+const metrics = new Metrics()
+
+module.exports = metrics
--- src/service.js
+++ src/service.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { authRouter, setAuthUser } = require('./routes/authRouter.js');
 const orderRouter = require('./routes/orderRouter.js');
 const franchiseRouter = require('./routes/franchiseRouter.js');
+const metrics = require('./metrics.js')
 const version = require('./version.json');
 const config = require('./config.js');
 
 const app = express();
 app.use(express.json());
+app.use(metrics.requestTracker.bind(metrics))
 app.use(setAuthUser);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
